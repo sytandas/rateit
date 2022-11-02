d7e082a20f87d60809ade9755aa46c27d35541d5
sayantan3 <sayantand975@gmail.com>
2022-11-02 22:02:30 +0530

    update

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
 // search bar - fetch info from imdb + rottentomatoes -> show info
-// fomula that calculate rateit rating
 // show output
 function movieResult(){ 
   var result = 1;
-  var imdb = document.getElementById("imdbRating").value;
-  var rotten = document.getElementById("rottenRating").value;
-  result = (imdb * 0.5) + (rottenRatingen * 0.5);
+  var imdb = document.getElementById("imdbRate").value;
+  var rotten = document.getElementById("rottenRate").value;
+  result = (imdb * 0.5) + (rotten * 0.5);
   document.getElementById("op").innerHTML = result;
 }
