a04f12cdc0e6ae18831b4217c7cff20b10ab39ad
sayantan3 <sayantand975@gmail.com>
2022-11-19 15:01:54 +0530

    memorization

--- src/knapsack.py
+++ src/knapsack.py
@@ -1,26 +1,30 @@
-# T - time watcher have
+# Time - time watcher have
 # time - time individual movies have
 # rating - movies rating
 # n - lenght of (rating)
+# using 2-D array with 2 rows O(2W)
+# Time coplexity O(N*W)
 
-def knapSack(Time, time, rating, n):
-    K = [[0 for x in range(Time+1)] for y in range(2)]
-    for i in range(n + 1):
-        for w in range(Time + 1):
-            if (i == 0 or w == 0):
-                K[i % 2][w] = 0
-            elif (time[i - 1] <= w):
-                K[i % 2][w] = max(
-                        rating[i - 1]
-                        +K[(i - 1) % 2][w - time[i - 1]],
-                        K[(i - 1) % 2][w])
-            else:
-                K[i % 2][w] = K[(i - 1) % 2][w]
-    return K[i % 2][Time]
 
+def knapSack(time, rating, Time, n):
+    # K = [[0 for x in range(Time+1)] for y in range(2)]
+    # base condtions 
+    table = [[-1 for i in range(Time + 1)] for j in range(n + 1)]
+    if (n == 0 or Time == 0):
+        return 0
+    if table[n][Time] != -1:
+        return table[n][Time]
+    # choices
+    if time[n - 1] <= Time:
+        table[n][Time] = max(rating[n - 1] + knapSack(time, rating, Time - time[n - 1], n - 1),
+                                                      knapSack(time, rating, Time, n - 1))
+        return table[n][Time]
+    elif time[n - 1] > Time:
+        table[n][Time] = knapSack(time, rating, Time, n - 1)
+        return table[n][Time]
 if __name__ == "__main__":
     rating = [10, 8, 8, 7]
     time = [50, 40, 20, 10]
     Time = 40
     n = len(rating)
-    print(knapSack(Time, time, rating, n))
+    print(knapSack(time, rating, Time, n))
